59d1bcd3449ad1ec58c49b72b83ab1cd6a706766
JuJuBayatz <laznimail1@gmail.com>
2024-09-19 21:01:48 +0200

    Update EndFlag.gd

--- Scenes/EndFlag.gd
+++ Scenes/EndFlag.gd
@@ -2,7 +2,7 @@ extends Area2D
 @export_file("*.tscn") var next_scene
 
 
-func _on_body_entered(body):
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		print("sdasd")
 		get_tree().change_scene_to_file(next_scene)
